5a8c5e149d4a5d445e48398bd833d7ca39cadf4e
Nermin-00 <71044724+Nermin-00@users.noreply.github.com>
2024-02-06 11:27:54 +0100

    Testing a version with GUI, using Tkinter
    Using tkinter to create a GUI window for ease of use.

--- main.py
+++ main.py
@@ -1,48 +1,63 @@
 #Importing library.
 import datetime
+#Importing tkinter  GUI
+from tkinter import *
 
 #Entry initiation function
-def entry():
-    
-        arivee = input("Enter who arrived : ")
+def printtext():
+    global e
+    #storing value of e.get into "string"
+    string = e.get(); 
+    #^prints the string
+    print (string)
 
-    #Concatecating and joining time and arivee variables.
-        info = arivee + " - " + realtime + "\n"
+        #Concatecating and joining time and arivee variables.
+    
 
     #Opening the logs.txt file to write, 'a' argument to continue on to already written file.
-        openlogs = open('logs.txt', 'a')
+        ##openlogs = open('logs.txt', 'a')
     #Actually writing into the file, from the info variable we made earlier.
-        openlogs.write(info)
+        ##openlogs.write(info)
 
-#Assigning time to a variable.
-time = datetime.datetime.now()
+    #Assigning time to a variable.
+        ##time = datetime.datetime.now()
 
 #Converting realtime output into a string for further processing.
-realtime = str(time)
+        ##realtime = str(time)
+root = Tk()
+e = Entry(root)
+e.pack()
+e.focus_set()
+
+root.geometry('400x200')
+
+##This is button funcitonality
+#When we click, we want to store text 
+#from input box to .txt file
+#our entry() function does that already
+#maybe we could call it.
+#We called it but it gets called on button event, and -expects prompt in the console-
+
+    
+#Assigning time to a variable.
+#time = datetime.datetime.now()
+
+#Converting realtime output into a string for further processing.
+#realtime = str(time)
 
 
-##Deciding between multiple user input during runtime.
-multipleUserInput = input("Multiple User Entry? Y/N ")
-if (multipleUserInput == "N" ):
-    print(" No multiple entry activated.")
-     #Prompting for arivee's name, function call.
-    entry()
+##entry1 = Entry(root, width=20)entry1.pack()
+
+Button(root, text="Enter user", command=printtext).pack()
+
+root.mainloop()
+
+######################################################
     
-    
-#Multiple User Entry during runtime functionality. 
-else:
-    print("Multiple User Entry Mode Activated!")
-    multipleEntry = input("Enter the amount of arivee's you'd like to add : ")
-    #Converting string into an int.
-    multipleEntryInt = int(multipleEntry)
-    
-#Calling the function the amount of times entered during Multiple User Entry
-for x in range (multipleEntryInt):
-    print(x)
-    entry()
-    
-    
-    
+
+
+
+
     
     
 
