a0a7ad15ef9442e2c0c62fc41231da3291a6a5f3
Nermin-00 <71044724+Nermin-00@users.noreply.github.com>
2024-02-11 07:21:33 +0100

    Full, working program with GUI
    The program now accepts names and prints them out in the box below, alongside the time it was input, made using Tkinter GUI library.

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
 from tkinter import *
+import datetime
  
 root = Tk()
-root.geometry("300x300")
-root.title(" Q&A ")
+root.geometry("600x600")
+root.title("Arrival Times Tracker ")
+root['bg']='#001118'
  
+ #on button click function initialization
 def Take_input():
     INPUT = inputtxt.get("1.0", "end-1c")
-    print(INPUT)
-    import datetime
+    
     REALTIME = datetime.datetime.now()
+    #converting date & time from integer to a string for handling
     TIME = str(REALTIME)
-    Output.insert(END, INPUT + ">AT TIME>")
+    Output.insert(END, INPUT + " > AT : > ")
     Output.insert(END, "")
     Output.insert(END, TIME)
-     
-l = Label(text = "Enter ")
-inputtxt = Text(root, height = 10,
+    Output.insert(END,"                   ")
+
+##functionality works     
+l = Label(text = "Enter the name", font=23, bg="#03a9f4")
+inputtxt = Text(root, height = 2,
+                font=25,
                 width = 25,
-                bg = "light yellow")
+                bg = "#0276ab")
  
 Output = Text(root, height = 5, 
-              width = 25, 
-              bg = "white")
+              width = 45, 
+              font=17,
+              bg = "#0276ab")
  
 Display = Button(root, height = 2,
                  width = 20, 
-                 text ="Show",
+                 font=11,
+                 bg="#0276ab",
+                 text ="ENTER",
                  command = lambda:Take_input())
  
 l.pack()
